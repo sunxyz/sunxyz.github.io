ec82545fc14abede89755822427e5775e1027a00
sunxyz <zhugeyangyang1994@gmail.com>
2023-03-21 13:47:58 +0800

    -

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://static.webfollow.cc/sw.js', { scope: 'https://static.webfollow.cc/' })})}
