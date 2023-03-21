bc34feb5f1e96784e43feb5f0693c798e73653c2
sunxyz <zhugeyangyang1994@gmail.com>
2023-03-21 10:03:37 +0800

    -

---
+++ assets/About.3d1e3d5c.js
@@ -0,0 +1,24 @@
+import{x as o,r as e,o as i,e as s}from"./index.98049273.js";const r={__name:"About",setup(a){const n=o({title:"\u300CAfter Lounge\u300D\u4E50\u5668\u5077\u5FC3\u8D3C/\u8428\u514B\u65AF\u4E0E\u6D41\u884C\u4E50\u878D\u5408\u97F3\u4E50\u6B4C\u5355",thumbnail:"http://cdn.lizhi.fm/audio_cover/2023/01/20/2987098257743054343_80x80.jpg",pubDateTime:"2023\u5E7401\u670828\u65E5",link:"https://www.lizhi.fm/dhp_static/static/h5_dhp_app/share.html#/voice?voiceId=2987098258342008326&podcastId=5148252485972740735",description:`<myfont margin="0" padding="0" font-family="NotoSansCJKsc" size="15sp" line-height="26dp" color="#2E323F" text-style="normal"> 
+ <p>\u52A0\u5165\u91D1\u6C64\u529B\u7535\u53F0\u542C\u53CB\u7FA4\uFF01&nbsp;</p> 
+</myfont>
+<myfont margin="0" padding="0" font-family="NotoSansCJKsc" size="15sp" line-height="26dp" color="#2E323F" text-style="normal"> 
+ <p>\u5FAE\u535A\u641C\u7D22\uFF1A[\u91D1\u6C64\u529B\u7535\u53F0] \u79C1\u4FE1 \u83B7\u53D6\u5165\u7FA4\u65B9\u5F0F\uFF5E\u200B&nbsp;&nbsp;&nbsp;</p> 
+</myfont>
+<myfont margin="0" padding="0" font-family="NotoSansCJKsc" size="15sp" line-height="26dp" color="#2E323F" text-style="normal"> 
+ <p>\u672C\u671F\u8282\u76EE\u7684\u97F3\u4E50\u7EAF\u4EAB\u6B4C\u5355\u5DF2\u5EFA\u7ACB\u5728\uFF1A \u300C\u7F51\u6613\u4E91\u8D26\u53F7\u300D</p> 
+</myfont>
+<myfont margin="0" padding="0" font-family="NotoSansCJKsc" size="15sp" line-height="26dp" color="#2E323F" text-style="normal"> 
+ <p>\u641C\u7D22\uFF1A\u91D1\u6C64\u529B\u7535\u53F0\uFF0C\u81EA\u5EFA\u6B4C\u5355\u4E2D\uFF01&nbsp;</p> 
+</myfont>
+<myfont margin="0" padding="0" font-family="NotoSansCJKsc" size="15sp" line-height="26dp" color="#2E323F" text-style="normal"> 
+ <p>\u5BF9\u5E94\u8282\u76EE\u540D\u5B57\u548C\u5C01\u9762\u53BB\u627E\u4F60\u559C\u6B22\u7684\u90A3\u9996\u6B4C\u5427~</p> 
+</myfont>
+<myfont margin="0" padding="0" font-family="NotoSansCJKsc" size="15sp" line-height="26dp" color="#2E323F" text-style="normal"> 
+ <p>\u5976\u9A8F\u559C\u6B22\u628A\u8428\u514B\u65AF\u98CE\u7684\u6F14\u594F\u6BD4\u4F5C\u4E50\u5668\u5077\u5FC3\u8D3C\uFF0C\u5F97\u76CA\u4E8E\u5B83\u7684\u97F3\u8272\uFF0C\u4E5F\u662F\u56E0\u4E3A\u8FD9\u4E2A\u4E50\u5668\u7279\u522B\u7684\u6F14\u594F\u6280\u5DE7\u3002</p> 
+</myfont>
+<myfont margin="0" padding="0" font-family="NotoSansCJKsc" size="15sp" line-height="26dp" color="#2E323F" text-style="normal"> 
+ <p>\u8FD9\u4E2A\u4E50\u5668\u4E00\u76F4\u6709\u5B83\u975E\u5E38\u795E\u5947\u7684\u9B45\u529B \u63D0\u5230\u4ED6\uFF0C\u6211\u4EEC\u53EF\u80FD\u9A6C\u4E0A\u4F1A\u60F3\u5230\u6D17\u8111\u795E\u66F2\u300C\u56DE\u5BB6\u300D\u6216\u8005\u662F\u4F60\u603B\u4F1A\u63D0\u5230\u7684\u5077\u5FC3\u8D3C\u5C5E\u6027\u7684\u540D\u66F2The One You Love.</p> 
+</myfont>
+<myfont margin="0" padding="0" font-family="NotoSansCJKsc" size="15sp" line-height="26dp" color="#2E323F" text-style="normal"> 
+ <p>\u8428\u514B\u65AF\u98CE\u878D\u5408\u8FDB\u6D41\u884C\u97F3\u4E50\u5F53\u4E2D\u4E5F\u540C\u6837\u6709\u81EA\u5DF1\u795E\u5947\u7684\u4F5C\u7528\uFF0C \u5B83\u603B\u5728\u6700\u6070\u5F53\u65F6\u5019\u51FA\u73B0\u77AC\u95F4\u6293\u4F4F\u6240\u6709\u4EBA\u7684\u8033\u6735\uFF0C\u90A3\u4ECA\u5929\u7684\u8282\u76EE\u6211\u4EEC\u5C31\u548C\u5927\u5BB6\u4E00\u8D77\u6765\u5206\u4EAB\u51E0\u9996\u6709\u8428\u514B\u65AF\u4F5C\u4E3A\u534F\u4F5C\u5668\u4E50\u7684\u97F3\u4E50\u3002</p> 
+</myfont>`,channelId:"http%3A%2F%2Frss.lizhi.fm%2Frss%2F14623290.xml",channelName:"\u91D1\u6C64\u529B\u7535\u53F0",enclosure:"http://cdn.lizhi.fm/audio/2023/01/20/2987098258342008326_hd.mp3",itunesDuration:"1:00:55",star:!1});return(p,l)=>{const t=e("c-m-player");return i(),s(t,{"song-info":n.value},null,8,["song-info"])}}};export{r as default};
